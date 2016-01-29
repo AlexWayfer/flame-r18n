da0bc67086cbb0525512a1022f5510776d0e65a3
Alexander Popov <alex.wayfer@gmail.com>
2016-01-29 16:29:18 +0300

    Init commit

---
+++ .gemspec
@@ -0,0 +1,21 @@
+Gem::Specification.new do |s|
+	s.name        = 'flame-r18n'
+	s.version     = '1.0.0'
+	s.date        = Date.today.to_s
+
+	s.summary     = 'R18n plugin for Flame-framework'
+	s.description = 'Flame extension which provides i18n support' \
+	                ' to translate your web application.' \
+	                ' It is a wrapper for R18n core library.' \
+	                ' See R18n documentation for more information.'
+
+	s.authors     = ['Alexander Popov']
+	s.email       = ['alex.wayfer@gmail.com']
+	s.homepage    = 'https://gitlab.com/AlexWayfer/flame-r18n'
+	s.license     = 'MIT'
+
+	s.add_dependency 'flame', '~> 4.0', '>= 4.0.0'
+	s.add_dependency 'r18n-core', '~> 2'
+
+	s.files = Dir[File.join('lib', '**', '*')]
+end
---
+++ lib/flame/r18n.rb
@@ -0,0 +1,53 @@
+require 'r18n-core'
+
+module Flame
+	# Module for Flame::R18n extension with helper methods and base class
+	module R18n
+		include ::R18n::Helpers
+
+		def self.config(app)
+			app.config[:default_locale] = proc { ::R18n::I18n.default }
+			app.config[:locales] = proc { ::R18n.default_places }
+			app.config[:locales_dir] = proc do
+				File.join(app.config[:root_dir], 'locales')
+			end
+
+			::R18n.default_places { app.config[:locales_dir] }
+		end
+
+		def execute(method)
+			load_r18n
+			super
+		end
+
+		private
+
+		def init_r18n
+			::R18n::I18n.new(
+				locales_from_env,
+				::R18n.default_places,
+				off_filters: :untranslated,
+				on_filters: :untranslated_html
+			)
+		end
+
+		def locales_from_env
+			locales = ::R18n::I18n.parse_http(request.env['HTTP_ACCEPT_LANGUAGE'])
+			if params[:locale]
+				locales.insert(0, params[:locale])
+			elsif session[:locale]
+				locales.insert(0, session[:locale])
+			end
+		end
+
+		def load_r18n
+			::R18n.clear_cache! if config[:environment] == 'development'
+
+			::R18n.thread_set do
+				::R18n::I18n.default = config[:default_locale] if config[:default_locale]
+
+				init_r18n
+			end
+		end
+	end
+end
