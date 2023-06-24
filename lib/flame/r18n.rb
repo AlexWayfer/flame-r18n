5a11f16582d8e25d2890a0786abd430496875d6c
Alexander Popov <alex.wayfer@gmail.com>
2023-06-24 17:31:43 +0300

    Resolve new RuboCop offenses

--- lib/flame/r18n.rb
+++ lib/flame/r18n.rb
@@ -2,7 +2,7 @@
 
 require 'r18n-core'
 
-require_relative './r18n/version'
+require_relative 'r18n/version'
 
-require_relative './r18n/initialization'
-require_relative './r18n/locale_in_path'
+require_relative 'r18n/initialization'
+require_relative 'r18n/locale_in_path'
